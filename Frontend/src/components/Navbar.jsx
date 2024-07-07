5e10625a38192265ded7bae435f9a146190286fa
Anushy <rohanjatasra055@gmail.com>
2024-07-07 17:06:13 +0530

    Backend Done !!

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react'
 import {useState} from "react"; 
 import Login from "./Login";
+import Logout from "./Logout";
+import { useAuth } from "../context/AuthProvider";
+
 
 function Navbar  ()  {
+
+      const [authUser,setAuthUser]=useAuth()
 
       const [theme, setTheme] = useState(localStorage.getItem("theme") ? localStorage.getItem("theme") : "light" ) ;
 
@@ -123,12 +128,17 @@ function Navbar  ()  {
   </svg>
 </label>
 
-  <div>
+{
+  authUser ? (
+  <Logout/>
+  ) : (
+      <div>
     <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer"
       onClick={()=>document.getElementById("my_modal_3").showModal()}>
       Login</a>
       <Login/>
-  </div>
+  </div> 
+  )}
 </div>
 </div>
 </div>
